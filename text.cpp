982e0ea82b6d4023bae8f9bd57f5bd09055e5b69
soumen255 <soumenbhandari88@gmail.com>
2024-11-13 18:31:19 +0000

    merge sort code add

---
+++ text.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to merge two halves
+void merge(vector<int> &arr, int left, int mid, int right)
+{
+    int n1 = mid - left + 1; // Size of left subarray
+    int n2 = right - mid;    // Size of right subarray
+
+    // Create temporary arrays
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    int i = 0;    // Initial index of first subarray
+    int j = 0;    // Initial index of second subarray
+    int k = left; // Initial index of merged subarray
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Function to implement merge sort
+void mergeSort(vector<int> &arr, int left, int right)
+{
+    if (left < right)
+    {
+        // Find the middle point
+        int mid = left + (right - left) / 2;
+
+        // Recursively sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+// Function to print an array
+void printArray(const vector<int> &arr)
+{
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+// Main function
+int main()
+{
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    cout << "Given array is: ";
+    printArray(arr);
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array is: ";
+    printArray(arr);
+
+    return 0;
+}
