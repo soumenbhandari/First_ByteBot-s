ee2a6169e07f4268cbc5e1825e8946e1f5c5decc
soumenbhandari <soumenbhandari25@gmail.com>
2024-11-15 21:09:32 +0530

    a code of slinding window proble

---
+++ kslidingWindos.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+
+class sliding
+{
+public:
+ queue<int> q;
+ vector<int>ans;
+ 
+   void windows(vector<int> &arr, int k)
+   {
+      int index = 0;
+      int size = arr.size();
+      while(index!=size){
+
+      if (q.empty())
+      {
+         for (int i = 0; i < k; i++)
+         {
+            q.push(arr[index]);
+            index++;
+         }
+         ans.push_back(display(q));
+      }
+      else
+      {
+            q.pop();
+            q.push(arr[index]);
+            index++;
+           ans.push_back(display(q));
+         }
+      
+      }
+   }
+   
+   int display(queue<int>q){
+       while (!q.empty()){
+           if(q.front()<0){
+               return q.front();
+           }
+           q.pop();
+       }
+       return 0;
+   }
+   
+   
+};
+
+int main()
+{
+   vector<int> arr = {3, -6, 2, -7, -8, 11};
+   sliding a;
+   int k = 3;  
+   a.windows(arr, k);
+   return 0;
+}
